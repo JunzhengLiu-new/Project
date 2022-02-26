686a33067c1c7a8cbe1f81b5ce87a57830129e00
JunzhengLiu-new <71883592+JunzhengLiu-new@users.noreply.github.com>
2022-02-26 11:16:15 +0000

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//entrance js
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+import Memory_utils from "./utils/Memory_utils"   
+import Storage_utils from "./utils/Storage_utils"
+const user = Storage_utils.getUser()//Open the page and sandwich the local data in the middle
+Memory_utils.user = {username:user}
+ReactDOM.render(<App/>,document.getElementById("root"))
